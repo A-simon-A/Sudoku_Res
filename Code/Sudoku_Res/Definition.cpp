ac36df5e7c5cb6179a6f15bff77ec84594f74880
Simon <simon.autechaud@gmail.com>
2021-08-26 22:18:55 +0200

    creation of Statement & Definition

--- Code/Sudoku_Res/Definition.cpp
+++ Code/Sudoku_Res/Definition.cpp
@@ -1,5 +1,5 @@
 //
-//  main.cpp
+//  Definition.cpp
 //  Sudoku_Res
 //
 //  Created by Simon AUTECHAUD on 26/08/2021.
@@ -7,9 +7,3 @@
 
 #include <iostream>
 #include "Statement.h"
-using namespace std ;
-
-int main() {
-    
-    return 0;
-}
---
+++ Code/Sudoku_Res/Statement.h
@@ -0,0 +1,12 @@
+//
+//  Statement.h
+//  Sudoku_Res
+//
+//  Created by Simon AUTECHAUD on 26/08/2021.
+//
+
+#ifndef Statement_h
+#define Statement_h
+
+
+#endif /* Statement_h */
